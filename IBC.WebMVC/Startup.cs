45899c5a8169296913074b349f24c37f544d8da1
dr orpheus <wyliepeach53@gmail.com>
2018-11-27 12:50:15 -0600

    Add project files.

---
+++ IBC.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IBC.WebMVC.Startup))]
+namespace IBC.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
