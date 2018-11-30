023bb5879b0779e9042405b77a77f6059789cf0d
dr orpheus <wyliepeach53@gmail.com>
2018-11-30 12:15:03 -0500

    X1 Edit Model

---
+++ IBC.Models/X1BladeModels/X1BladeEdit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBC.Models.X1BladeModels
+{
+    class X1BladeEdit
+    {
+    }
+}
